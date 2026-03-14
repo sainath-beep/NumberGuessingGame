4877e56ce3050f9d18bad2d12b0da84ff3654c6a
sainath biradar <sainathb754@gmail.com>
2026-03-14 22:18:09 +0530

    mini project done

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -7,17 +7,17 @@ public class NumberGuessingGame {
 
         System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("Try to guess the number I'm thinking of between 0 and 99.");
-        System.out.println("Good luck!");
+        System.out.println("Good luck! you have 7 guesses to find the secret number.");
 
         System.out.println("----------------------------------------------");
         Scanner scanner = new Scanner(System.in);
+        
         int geusses = 1;
         int userNumber;
         do{
             System.out.print("Enter a number between 0 and 99:");
-
             userNumber=scanner.nextInt();
-
+            
             if(geusses==7){
                 System.out.println("Sorry, you've used all your guesses. The secret number was: " + secretNumber);
                 break;
@@ -32,7 +32,6 @@ public class NumberGuessingGame {
                 System.out.println("Congratulations! You've guessed the secretNumber.");
             }
             geusses++;
-
         }while(userNumber!=secretNumber);
         scanner.close();
     }
--- src/SudokuSolver.java
+++
@@ -1,6 +0,0 @@
-public class SudokuSolver {
-    public static void main(String[] args) {
-        System.out.println("Sudoku Solver is not yet implemented.");
-
-    }
-}
