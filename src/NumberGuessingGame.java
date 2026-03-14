0063344483d84e16557592079fffe2bba5c7a8bf
sainath biradar <sainathb754@gmail.com>
2026-03-14 15:30:35 +0530

    just added some limitation of geussing

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -11,19 +11,28 @@ public class NumberGuessingGame {
 
         System.out.println("----------------------------------------------");
         Scanner scanner = new Scanner(System.in);
-
+        int geusses = 1;
         int userNumber;
         do{
             System.out.print("Enter a number between 0 and 99:");
 
             userNumber=scanner.nextInt();
-            if(userNumber<secretNumber){
+
+            if(geusses==7){
+                System.out.println("Sorry, you've used all your guesses. The secret number was: " + secretNumber);
+                break;
+            }
+             else if(userNumber<0 || userNumber>99){
+                System.out.println("Invalid input. Please enter a number between 0 and 99.");
+            } else if(userNumber<secretNumber ){
                 System.out.println(" The number is higher.");
             }else if(userNumber>secretNumber){
                 System.out.println(" The number is lower.");
             }else{
                 System.out.println("Congratulations! You've guessed the secretNumber.");
             }
+            geusses++;
+
         }while(userNumber!=secretNumber);
         scanner.close();
     }
